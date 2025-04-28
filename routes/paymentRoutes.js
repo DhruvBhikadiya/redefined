b75950b646d5b7d60a81897cbfb6eadda4f2b255
Dhruv Bhikadiya <138368408+DhruvBhikadiya@users.noreply.github.com>
2025-04-28 14:27:55 +0530

    Delete routes directory

--- routes/paymentRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createPaymentIntent, updatePaymentData } = require('../controllers/paymentController');
-
-router.post('/createPayment', createPaymentIntent);
-router.post('/updatePayment', updatePaymentData);
-module.exports = router;
