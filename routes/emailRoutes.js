1f9c565de41d252f8db62e8ed2f008c437f8da3f
Dhruv Bhikadiya <138368408+DhruvBhikadiya@users.noreply.github.com>
2025-05-04 18:20:09 +0530

    Delete routes directory

--- routes/emailRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { sendEmailController } = require('../controllers/emailController');
-
-router.post('/send', sendEmailController);
-
-module.exports = router;
