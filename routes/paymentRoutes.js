a6def3186d7eba9cc959450b304947df413a7828
Dhruv Bhikadiya <138368408+DhruvBhikadiya@users.noreply.github.com>
2025-05-10 14:37:19 +0530

    Delete routes directory

--- routes/paymentRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createPaymentIntent, updateEventPaymentData, createEventPaymentIntent, updatePaymentData } = require('../controllers/paymentController');
-
-router.post('/createPayment', createPaymentIntent);
-router.post('/updatePayment', updatePaymentData);
-router.post('/createEventPayment', createEventPaymentIntent);
-router.post('/updateEventPaymentData', updateEventPaymentData);
-module.exports = router;
--- routes/subscriptionRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const subscriptionController = require('../controllers/subscriptionController');
-const webhook = require('../webhook/webhook');
-
-router.post('/create-subscription', subscriptionController.createSubscription);
-router.post('/webhook', express.raw({ type: 'application/json' }), webhook.handleStripeWebhook);
-
-module.exports = router;
